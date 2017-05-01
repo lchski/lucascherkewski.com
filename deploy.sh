d9a5f13f39ef4ff15bbbb05b7042233a5529d45c
Lucas Cherkewski <lucas@ecustom.ca>
2017-04-30 22:28:59 -0400

    spiff up deploy script; one step process now!

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,12 @@
 #!/bin/zsh
+ARCHIVE_NAME="lucascherkewski.com.tar.gz"
+DEPLOY_PATH="${LCCOM_SERVER_PATH}/ansible/resources/"
+
 bundle exec jekyll b
 cd _site
-#tar -czf lucascherkewski.com.tar.gz *
+tar -czf $ARCHIVE_NAME *
+rm $DEPLOY_PATH$ARCHIVE_NAME
+mv lucascherkewski.com.tar.gz $DEPLOY_PATH
+cd $LCCOM_SERVER_PATH
+debops --tags role::resources:archives
+
