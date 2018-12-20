505a8ddfd45a3b6fa565a9fa0f7d2776669bb53c
Lucas Cherkewski <lucas@ecustom.ca>
2018-12-20 16:54:38 -0500

    update new draft template
    BYE BYE INLINE CSS

--- new-draft.sh
+++ new-draft.sh
@@ -14,12 +14,6 @@ categories:
 published: true
 ---
 
-{:h2: .f6 .ttu .tracked .mt5 .lh-title}
-{:link: .dim .light-purple}
-{:blockquote: .ml0 .pl4 .bl .bw3 .b--black-10 .f4}
-{:blockquote__citation: .f5 .measure}
-{:hr: .w-40 .mv5 .bw1 .ba .b--black-10}
-
 END
 )
 
